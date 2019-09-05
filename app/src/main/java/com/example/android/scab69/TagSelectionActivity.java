4f064caf9b1c43e7a8927793a6d521c337d7ebfb
Priyesh <ekthapriyesh24@gmail.com>
2019-09-05 17:58:21 +0530

    added new activities

---
+++ app/src/main/java/com/example/android/scab69/TagSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.scab69;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TagSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag_selection);
+    }
+}
